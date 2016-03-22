1603cab93344da0d6b79a70fc0ecb94b5cdadeb4
Nelson <nelson@nelsontran.com>
2016-03-22 18:30:46 -0400

    Refactor database and parser functionality out of league_mmr.py
    + Allow users to specify table name in the JSON configuration file.
    + Checks if the JSON configuration file is valid during runtime.
    + Checks if the `date` string matches the SQL date type format.
    + Checks if the region abbreviation read from the summoner list is
      a valid one that is supported by op.gg.
    + Added docstring comments.

---
+++ database.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+
+import json
+import re
+import mysql.connector
+
+class LeagueDatabase:
+
+    sql_config_attrs = [
+        "user",
+        "password",
+        "host",
+        "port",
+        "database",
+        "table"]
+
+    sql_connection = None
+    sql_cursor = None
+    sql_config = {}
+    
+    mmr_table = ""
+
+    def __init__(self, config):
+        """
+        Construct a LeagueDatabase object and load configuration from
+        a file.
+
+        Args:
+            config (str): The file path to a JSON file that contains
+                all of the configuration information needed to connect
+                to an SQL database.
+        """
+
+        # load configuration data from file
+        with open(config) as json_config:
+            self.sql_config = json.load(json_config)
+
+            if (not set(self.sql_config.keys()) <= set(self.sql_config_attrs)):
+                raise ValueError("Invalid SQL config file")
+
+        # the `table` attribute is not needed for connecting to a
+        # database using the MySQL connector so we will move it out
+        # of the dictionary into its own variable
+        self.mmr_table = self.sql_config.pop("table")
+
+
+    def add_row(self, summoner, region, mmr, date):
+        """
+        Insert MMR data for a summoner into the MMR table in the
+        database.
+
+        Args:
+            summoner (str): The name of the summoner.
+            region (str): The server/region abbreviation.
+            mmr (int): The MMR of the summoner.
+            date (str): The date associated with the summoner/MMR.
+
+        Returns:
+            True if the transaction was successful; False otherwise.
+
+        Raises:
+            ValueError: If `date` does not match the format of the MySQL
+                date type format (YYYY-MM-DD).
+        """
+
+        is_success = True
+
+        date_regex_pattern = "^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]"
+        if (not re.match(date_regex_pattern, date)):
+            raise ValueError("Invalid date format, should be YYYY-MM-DD")
+
+        try:
+            add_mmr_record = (
+                "INSERT INTO " + self.mmr_table + " "
+                "(summoner, region, mmr, date) "
+                "VALUES (%s, %s, %s, %s)")
+
+            mmr_record = (summoner, region, mmr, date)
+            self.sql_cursor.execute(add_mmr_record, mmr_record)
+            self.sql_connection.commit()
+            
+        except mysql.connector.errors.IntegrityError:
+            # most likely due to already existing record for the date;
+            # only one MMR record per day per summoner
+            is_success = False
+
+        return is_success
+
+
+    def connect(self):
+        """
+        Connect to a MySQL database using the configuration loaded in
+        from the constructor and then check to see if a table exists
+        for inserting MMR data. If not, create that table.
+        """
+
+        self.sql_connection = mysql.connector.connect(**self.sql_config)
+        self.sql_cursor = self.sql_connection.cursor()
+
+        if (not self.__table_exists()):
+            self.__create_table()
+
+
+    def close(self):
+        """
+        Close all database connection resources.
+        """
+
+        self.sql_cursor.close()
+        self.sql_connection.close()
+
+
+    def __create_table(self):
+        """
+        Create a new table in the database with the following schema:
+        (SUMMONER, REGION, MMR, DATE). The primary key is defined to be
+        (SUMMONER, REGION, DATE). The name of the database will be the
+        same as the one specified in the JSON configuration file.
+        """
+
+        self.sql_cursor.execute(
+            "CREATE TABLE `" + self.mmr_table + "` ("
+            "  `summoner` VARCHAR(16) NOT NULL,"
+            "  `region` VARCHAR(4) NOT NULL,"
+            "  `mmr` INT NOT NULL,"
+            "  `date` DATE NOT NULL,"
+            "  PRIMARY KEY (`summoner`, `region`, `date`),"
+            "    KEY `summoner` (`summoner`),"
+            "    KEY `region` (`region`),"
+            "    KEY `date` (`date`)"
+            ") ENGINE=InnoDB")
+
+
+    def __table_exists(self):
+        """
+        Check if the table specified in the JSON configuration file
+        exists in the database. Returns True if it exists; False
+        otherwise.
+        """
+
+        self.sql_cursor.execute("SHOW TABLES LIKE '" + self.mmr_table + "'")
+        results = self.sql_cursor.fetchall()
+
+        if (len(results) > 0):
+            return True
+        else:
+            return False
---
+++ parser.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+import html.parser
+import string
+import urllib.request
+
+class MMRParser(html.parser.HTMLParser):
+
+    league_regions = [
+        "kr",   # Korea
+        "euw",  # Europe West
+        "oce",  # Oceania
+        "las",  # Latin America South
+        "ru",   # Russia
+        "na",   # North America
+        "eune", # Europe Nordic & East
+        "br",   # Brazil
+        "lan",  # Latin America North
+        "tr"]   # Turkey
+
+    found_tag = False
+    mmr = 0
+
+    def get_mmr(self, summoner, region):
+        """
+        Get the MMR of a summoner in a region by sending a GET request
+        to op.gg and parsing the <text/html> response.
+
+        Args:
+            summoner (str): The name of the summoner.
+            region (str): The abbreviation of the server/region that the
+                summoner belongs to.
+
+        Returns:
+            int: The MMR of the summoner. This value will be 0 if
+                 (1) the summoner is not currently ranked or
+                 (2) the GET request failed.
+
+
+        Raises:
+            ValueError: A non-existent or unsupported region is passed
+                in as an argument through `region`.
+        """
+
+        # reset HTML parser
+        self.reset()
+        self.mmr = 0
+
+        # normalize arguments
+        summoner = summoner.strip().lower().replace(' ', '%20')
+        region = region.strip().lower()
+
+        if (region not in self.league_regions):
+            raise ValueError("Region does not exist")
+
+        # construct op.gg GET request URL
+        opgg = "http://"
+        if (region != "kr"):
+            opgg += region + '.'
+        opgg += "op.gg/summoner/ajax/mmr/summonerName=" + summoner
+
+        # parse text/html response
+        request = urllib.request.urlopen(opgg)
+        self.feed(request.read().decode("utf-8"))
+        request.close()
+
+        return self.mmr
+
+    def handle_starttag(self, tag, attrs):
+        """
+        Ignore all HTML tags until <div class="MMR"> is found. Then,
+        set a flag to let handle_data(data) know to parse the the MMR.
+        """
+
+        if (tag == "div" and attrs[0][0] == "class" and attrs[0][1] == "MMR"):
+            self.found_tag = True
+
+    def handle_endtag(self, tag):
+        """
+        Set `found_tag` flag to False so handle_data(data) stops parsing
+        the content of subsequent HTML tags.
+        """
+        
+        self.found_tag = False
+
+    def handle_data(self, data):
+        """
+        Parse the HTML tag content the position on the parser pointer is
+        inside of the <div class="MMR"> tag.
+        """
+
+        if (self.found_tag):
+            self.mmr = int(data.strip().replace(',', ''))
